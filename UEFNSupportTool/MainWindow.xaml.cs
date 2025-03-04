2143915b21df2dc9a95d4e41ee1139107b49d26a
Gorosuke / ごろ助 <GorosukeSan56@gmail.com>
2025-03-04 23:29:54 +0900

    VerseTagタブを分離

--- UEFNSupportTool/MainWindow.xaml.cs
+++ UEFNSupportTool/MainWindow.xaml.cs
@@ -8,7 +8,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using UEFNSupportTool.Util;
 
 namespace UEFNSupportTool;
 
@@ -24,30 +23,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void Button_Convert(object sender, RoutedEventArgs e)
-    {
-        string newText = VerseComponent.PutVerseComponent(TextBox.Text);
-        TextBox.Text = newText;
-    }
-
-    private void ButtonBase_Copy(object sender, RoutedEventArgs e)
-    {
-        Clipboard.SetData(DataFormats.Text, TextBox.Text);
-    }
-
-    private void ButtonBase_Clear(object sender, RoutedEventArgs e)
-    {
-        TextBox.Text = string.Empty;
-    }
-
-    private void ButtonBase_Paste(object sender, RoutedEventArgs e)
-    {
-        string? text = Clipboard.GetData(DataFormats.Text).ToString();
-        if (text != null)
-        {
-            TextBox.Text = text;
-        }
     }
 }
---
+++ UEFNSupportTool/Views/VerseTag/VerseTag.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using UEFNSupportTool.Models.VerseTag;
+
+namespace UEFNSupportTool.Views.VerseTag;
+
+public partial class VerseTag : UserControl
+{
+    public VerseTag()
+    {
+        InitializeComponent();
+    }
+    
+    private void Button_Convert(object sender, RoutedEventArgs e)
+    {
+        string newText = VerseComponent.PutVerseComponent(TextBox.Text);
+        TextBox.Text = newText;
+    }
+
+    private void ButtonBase_Copy(object sender, RoutedEventArgs e)
+    {
+        Clipboard.SetData(DataFormats.Text, TextBox.Text);
+    }
+
+    private void ButtonBase_Clear(object sender, RoutedEventArgs e)
+    {
+        TextBox.Text = string.Empty;
+    }
+
+    private void ButtonBase_Paste(object sender, RoutedEventArgs e)
+    {
+        string? text = Clipboard.GetData(DataFormats.Text).ToString();
+        if (text != null)
+        {
+            TextBox.Text = text;
+        }
+    }
+}
